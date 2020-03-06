41e7ea6b7c8e1c9c098e3329301c0b8f911d5b9a
Noah Betzen <noah.betzen@workiva.com>
2020-03-06 15:13:00 -0600

    Update colors

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -67,8 +67,8 @@ export default class Index extends React.Component {
                 className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
                 style={{
                   boxShadow:
-                    'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-                  backgroundColor: 'rgb(255, 68, 0)',
+                    '#39DC78 0.5rem 0px 0px, #39DC78 -0.5rem 0px 0px',
+                  backgroundColor: '#39DC78',
                   color: 'white',
                   lineHeight: '1',
                   padding: '0.25em',
@@ -80,8 +80,8 @@ export default class Index extends React.Component {
                 className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
                 style={{
                   boxShadow:
-                    'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-                  backgroundColor: 'rgb(255, 68, 0)',
+                    '#39DC78 0.5rem 0px 0px, #39DC78 -0.5rem 0px 0px',
+                  backgroundColor: '#39DC78',
                   color: 'white',
                   lineHeight: '1',
                   padding: '0.25em',
