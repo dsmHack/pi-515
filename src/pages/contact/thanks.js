358b29395b4e5929c24608d240838da9158295fc
Sean Rennich <rennichs1@central.edu>
2020-03-07 12:03:27 -0600

    centered content

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -4,10 +4,12 @@ import Layout from '../../components/Layout'
 export default () => (
   <Layout>
     <section className="section">
-      <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+      <div className="columns is-centered is-vcentered">
+        <div class="column is-narrow has-text-centered">
+          <div className="content">
+            <h1>Thank you!</h1>
+            <p>Your request was submitted successfully. We will get back to you as soon as we can!</p>
+          </div>
         </div>
       </div>
     </section>
