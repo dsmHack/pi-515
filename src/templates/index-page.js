11884b485ee954831bb05799b2bca2a18b1c5340
LeftoverLemon59 <rennichs1@central.edu>
2020-03-07 16:02:09 -0600

    updated contact and volunteer (#35)
    +1

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -137,7 +137,7 @@ export const IndexPageTemplate = ({
                       className="button is-primary is-large">DONATE
                     </Link>
                     <Link 
-                      onClick={() => window.open('','_blank')}
+                      to="/contact"
                       className="button is-primary is-large is-light">VOLUNTEER
                   </Link>
                   </div>
