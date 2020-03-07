199343785856bf8df9abb51c586f4a354c2e0a9c
Stefan Kraus <stefan.kraus@workiva.com>
2020-03-07 17:38:17 -0600

    merge fix

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -10,7 +10,7 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/hero.png')`,
+            backgroundImage: `url('/img/blog_background.png')`,
           }}
         >
           <h1
